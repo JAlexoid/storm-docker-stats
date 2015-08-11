da9c347521bdb7c1fbe4562caa8fc856e4bc79db
Aleksandr Panzin <alex@panz.in>
2015-08-11 16:58:52 -0400

    Adding Graphite metrics collector for a Docker Storm deployment

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-docker build -t="wurstmeister/storm" storm
-docker build -t="wurstmeister/storm-nimbus" storm-nimbus
-docker build -t="wurstmeister/storm-supervisor" storm-supervisor
-docker build -t="wurstmeister/storm-ui" storm-ui
+mkdir __stg-tmp
+git clone git@github.com:verisign/storm-graphite.git __stg-tmp
+cd __stg-tmp
+git checkout 0.2.3
+./gradlew build
+mv build/libs/storm-graphite-0.2.3-SNAPSHOT-all.jar ../storm/
+cd ..
+rm -rf __stg-tmp
+
+docker build -t="jalexoid/storm:0.9.5" storm
+docker build -t="jalexoid/storm-nimbus:0.9.5" storm-nimbus
+docker build -t="jalexoid/storm-supervisor:0.9.5" storm-supervisor
+docker build -t="jalexoid/storm-ui:0.9.5" storm-ui
